884d68e0677982f6888ce958c340503186a3c91f
evencoding <88191233+evencoding@users.noreply.github.com>
2022-01-03 05:43:38 +0900

    Finishing Challenge

--- src/server.js
+++ src/server.js
@@ -14,51 +14,31 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
 function countRoom(roomName) {
   return wsServer.sockets.adapter.rooms.get(roomName)?.size;
 }
 
 wsServer.on("connection", (socket) => {
-  // wsServer.socketsJoin("announcement");
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(wsServer.sockets.adapter);
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    wsServer.sockets.emit("room_change", publicRooms());
+    socket.to(roomName).emit("welcome", countRoom(roomName));
   });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
   });
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
   });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
   });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+  socket.on("full", (roomName, done) => {
+    if (countRoom(roomName) !== 2) {
+      done();
+    } else {
+      socket.emit("isfull");
+    }
+  });
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:5000`);
